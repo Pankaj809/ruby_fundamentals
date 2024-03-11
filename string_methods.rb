8c5971a9bf062ee94db3c95d84d21a08ed325415
Pankaj809 <pankajadhikari809@gmail.com>
2024-03-11 22:16:26 +0545

    Added ruby fundamentals project

---
+++ string_methods.rb
@@ -0,0 +1,13 @@
+# puts "this is a string".upcase
+# puts "this is a string".downcase
+# puts "this is a string".reverse
+# puts "this is a string".capitalize
+# puts "this is a string".include?("ri")
+# puts "this is a string".include?("string")
+# puts "this is a string".split("this")
+# puts "this is a string".split(" ")
+# puts "10".to_i
+# puts "10".to_f
+
+puts "I love coding".include?("code")
+puts "how long is this?".length

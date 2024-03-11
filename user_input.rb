e8d4ce2de02da1580e871f69ece8b689b6c9383d
Pankaj809 <pankajadhikari809@gmail.com>
2024-03-11 22:42:56 +0545

    Getting input from user

---
+++ user_input.rb
@@ -0,0 +1,7 @@
+#Gets method is used to get the input from the user. It is used to read the input from the user and store it in a variable.
+puts "Enter your name: "
+name=gets
+puts "Your name is #{name}"
+puts "Enter your age: "
+age=gets
+puts "Your age is #{age}"

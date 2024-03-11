8ab9cc0c448cc58b013dd3feb8c4eab9b9d7e8ea
Pankaj809 <pankajadhikari809@gmail.com>
2024-03-12 00:05:06 +0545

    The array list

---
+++ quotes.rb
@@ -0,0 +1,42 @@
+# quotes=[]
+# puts "Here are #{quotes.length} quotes"
+
+# quote="Shine like sun"
+# quotes.push(quote)
+# puts "There are #{quotes.length} quotes"
+
+# quote="Be happy"
+# quotes.push(quote)
+# puts "There are #{quotes.length} quotes"
+
+# quote="Be kind"
+# quotes.push(quote)
+# puts "There are #{quotes.length} quotes"
+
+# random_quote=quotes.sample
+
+# puts "the random quote at index #{quotes.index(random_quote)} is: #{random_quote}"
+
+#here are the list of my subjects at college
+
+subjects=[]
+subject="Math"
+subjects.push(subject)
+subject="Science"
+subjects.push(subject)
+subject="C programming"
+subjects.push(subject)
+subject="Java"
+subjects.push(subject)
+subject="Python"
+subjects.push(subject)
+subject="Numerical Method"
+subjects.push(subject)
+subject="Data Structure"
+subjects.push(subject)
+subject="Database"
+subjects.push(subject)
+subject="Web Development"
+subjects.push(subject)
+random_subject=subjects.sample
+puts "The random subject at index #{subjects.index(random_subject)} is: #{random_subject}"
--- user_input.rb
+++ user_input.rb
@@ -1,7 +1,22 @@
-#Gets method is used to get the input from the user. It is used to read the input from the user and store it in a variable.
+# #Gets method is used to get the input from the user. It is used to read the input from the user and store it in a variable.
+# puts "Enter your name: "
+# name=gets
+# puts "Your name is #{name}"
+# puts "Enter your age: "
+# age=gets
+# puts "Your age is #{age}"
+# roll=gets
+# puts "your roll is #{roll}"
+# puts "Enter your name"
+# name=gets
+# puts "Hello #{name}! you are a future developer"
+
+# puts "Enter your lastname: "
+# name=gets.chomp
+# puts"Hello #{name}! you are a future developer"
+
 puts "Enter your name: "
-name=gets
-puts "Your name is #{name}"
-puts "Enter your age: "
-age=gets
-puts "Your age is #{age}"
+name=gets.chomp
+puts "What is your favorite food"
+food=gets.chomp
+puts "Hello #{name}! your favorite food is #{food}"

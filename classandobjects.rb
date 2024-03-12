b8795e2a23675bf3696db1ede8b56ac314bff8ad
Pankaj809 <pankajadhikari809@gmail.com>
2024-03-12 14:58:04 +0545

    instance variables basic

---
+++ classandobjects.rb
@@ -0,0 +1,22 @@
+# class Dog
+#     def initialize(name, breed)
+#         @name = name
+#         @breed = breed
+#     end
+# end
+
+
+# barnie= Dog.new("Barnie", "Golden Retriever")
+# puts barnie.inspect  # Output
+
+# cynthia= Dog.new("Cynthia", "Poodle")
+# puts cynthia.inspect
+
+
+class Car
+    def initialize(brand, color, maximum_passenger,max_speed)
+        @brand = brand
+        @color = color
+        @maximum_passenger = maximum_passenger
+        @max_speed = max_speed
+    end

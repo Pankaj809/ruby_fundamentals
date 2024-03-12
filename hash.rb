de9a83220ab9ef1e6515dcc662afb1ceb8ec65f0
Pankaj809 <pankajadhikari809@gmail.com>
2024-03-12 11:18:02 +0545

    Hash data structure

---
+++ hash.rb
@@ -0,0 +1,33 @@
+countries={
+    "Nepal" =>{
+        "Population"=>"3 crore",
+        "GDP"=>"$30 billion",
+        "Capital"=>"Kathmandu"
+    },
+
+    "India"=>{
+        "Population"=>"130 crore",
+        "GDP"=>"$2.6 trillion",
+        "Capital"=>"New Delhi"
+    },
+
+    "China"=>{
+        "Population"=>"140 crore",
+        "GDP"=>"$14 trillion",
+        "Capital"=>"Beijing"
+    },
+
+    "Japan"=>{
+        "Population"=>"12 crore",
+        "GDP"=>"$5 trillion",
+        "Capital"=>"Tokyo"
+    
+    },
+
+    "USA"=>{
+        "Population"=>"32 crore",
+        "GDP"=>"$20 trillion",
+        "Capital"=>"Washington D.C"
+        }
+}
+ puts  "#{countries["Nepal"]["Population"]}"

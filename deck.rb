e321196116c590bda0dc9f35ff0948f1a9b79b4b
Pankaj809 <pankajadhikari809@gmail.com>
2024-03-13 13:22:23 +0545

    solved deck and ordinal

--- deck.rb
+++ deck.rb
@@ -5,36 +5,40 @@ class Card
         @suit = suit
     end
 
+    def to_s
+        "#{@rank} of #{@suit}"
+      end
+    
+    
+
 end
     
     class Deck
-    attr_accessor :cards
-    def initialize
-        @cards = []
-        ranks = %w{2 3 4 5 6 7 8 9 10 J Q K A}
-        suits = %w{Spades Hearts Diamonds Clubs}
-        suits.each do |suit|
-        ranks.each do |rank|
-            @cards << Card.new(rank, suit)
+        attr_accessor :cards
+        def initialize
+            @cards = []
+            ranks = %w{2 3 4 5 6 7 8 9 10 J Q K A}
+            suits = %w{Spades Hearts Diamonds Clubs}
+            suits.each do |suit|
+            ranks.each do |rank|
+                @cards << Card.new(rank, suit)
+            end
+            end
         end
+
+
+        def shuffle
+            @cards.shuffle!
         end
-    end
-    def shuffle
-        @cards.shuffle!
-    end
-    def deal
-        @cards.shift
-    end
+
+        def deal
+            @cards.shift
+        end
+
+        
     end
     
     deck = Deck.new
     deck.shuffle
-    deck.cards.each do |card|
-    puts "#{card.rank} of #{card.suit}"
-    end
-    puts "----"
     puts deck.deal
-    puts "----"
-    deck.cards.each do |card|
-    puts "#{card.rank} of #{card.suit}"
-end
+
--- ruby_fundamentals_challenges-master/ordinal/lib/ordinal.rb
+++ ruby_fundamentals_challenges-master/ordinal/lib/ordinal.rb
@@ -1,12 +1,19 @@
 def ordinal(number)
   # your code here
-  if number.to_s.end_with?("1") && number != 11 
+  if number % 100 == 11 || number % 100 == 12 || number % 100 == 13
+    return "#{number}th"
+  elsif number.to_s.end_with?("1")
     return "#{number}st"
-  elsif number.to_s.end_with?("2") && number != 12 && number != 112 && number != 1012
+  elsif number.to_s.end_with?("2")
     return "#{number}nd"
-  elsif number.to_s.end_with?("3") && number != 13 && number != 213
+  elsif number.to_s.end_with?("3")
     return "#{number}rd"
   else
     return "#{number}th"
   end
-end 
+end
+
+puts ordinal(22)
+
+
+
